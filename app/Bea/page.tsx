51b706459fc4f834dfb863f05432870779ae6968
Gloria Beth Nola <gloriabethnola3399@gmail.com>
2025-09-16 19:54:52 +0800

    Gloria Beth Nola page

---
+++ app/Bea/page.tsx
@@ -0,0 +1,12 @@
+import Info from "./info";
+export default function Page(){
+   return (
+      <div>
+          <h1 className="text-3x1 font-bold">Gloria Beth Nola</h1>  
+   <Info />
+      </div>
+     
+   );
+
+
+}
